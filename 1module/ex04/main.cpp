6a24bf9fed5bdcb07deeca318b4498a717fcab0b
bseven <2494789@mail.ru>
2022-02-20 20:02:43 +0300

    ex04done

--- 1module/ex04/main.cpp
+++ 1module/ex04/main.cpp
@@ -1,55 +1,59 @@
 #include <iostream>
 #include <sstream>
-#include <regex>
 #include <fstream>
+#include <cstring>
 
 int main(int argc, char *argv[])
 {
     if (argc != 4)
     {
         std::cerr << "Invalid arguments\n";
-        // exit(1);
         return 1;
     }
 
     std::string fileName = argv[1];
-    std::ifstream fileStream(fileName);
-    if (!fileStream.is_open())
+    std::ifstream fileStream;
+    
+    fileStream.open(fileName.c_str());
+    if (!fileStream)
     {
         std::cerr << "File not opening" << std::endl;
-        // exit(1);
+        return 1;
+    }
+
+    std::ofstream replacedFileStream;
+
+    replacedFileStream.open((fileName + ".replace").c_str(), std::ios::trunc);
+    if (!replacedFileStream)
+    {
+        fileStream.close();
+        std::cerr << "Can't open created file" << std::endl;
         return 1;
     }
     
-    std::string replaceFrom = argv[2];
+    std::string const replaceFrom = argv[2];
     std::string replaceTo = argv[3];
+    std::string line;
 
+    while (std::getline(fileStream, line)){
 
-    std::stringstream stringStream;
+        size_t n = strlen(replaceFrom.c_str());
+        size_t pos = line.find(replaceFrom.c_str());
 
-    stringStream << fileStream.rdbuf();
+        while (pos != std::string::npos){
 
-    std::string fileContent = stringStream.str();
+            line.erase(pos, n);
+            line.insert(pos, replaceTo);
+            pos += strlen(replaceTo.c_str());
+            pos = line.find(replaceFrom.c_str(), pos, n);
+        }
 
-    size_t findResultPosition = 0;
-
-    while ((findResultPosition = fileContent.find(replaceFrom)) != std::string::npos)
-    {
-        fileContent.erase(findResultPosition, replaceFrom.length());
-        fileContent.insert(findResultPosition, replaceTo);
+        replacedFileStream << line;
+        if (!fileStream.eof())
+            replacedFileStream << '\n';
     }
-
-    std::ofstream replacedFileStream(fileName + ".replace");
-
-    if (!replacedFileStream.is_open())
-    {
-        std::cerr << "Can't open created file" << std::endl;
-        // exit(1);
-        return 1;
-    }
-
-    replacedFileStream << fileContent;
     fileStream.close();
-    replacedFileStream.close();
+    replacedFileStream.close();   
+
     return 0;
 }
